f18d05a1557930d2d5bc21061815c4143c1fe064
Gaurav Sarkar <60042820+GauravSarkar@users.noreply.github.com>
2021-07-29 00:19:51 +0530

    Update run_jointIDSF_PhoBERTencoder.sh

--- run_jointIDSF_PhoBERTencoder.sh
+++ run_jointIDSF_PhoBERTencoder.sh
@@ -1,5 +1,5 @@
 #As we initialize JointIDSF from JointBERT, user need to train a base model JointBERT first
-./run_jointBERT-CRF_PhoBERTencoder.sh
+bash /content/JointSDIC/experiment.sh
 #Train JointIDSF
 export lr=4e-5
 export c=0.15
@@ -8,16 +8,16 @@ echo "${lr}"
 export MODEL_DIR=JointIDSF_PhoBERTencoder
 export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
 echo "${MODEL_DIR}"
-python3 main.py --token_level word-level \
+python3 /content/JointSDIC/main.py --token_level word-level \
                   --model_type phobert \
                   --model_dir $MODEL_DIR \
-                  --data_dir PhoATIS \
+                  --data_dir /content/JointSDIC/PhoATIS \
                   --seed $s \
                   --do_train \
                   --do_eval \
                   --save_steps 140 \
                   --logging_steps 140 \
-                  --num_train_epochs 50 \
+                  --num_train_epochs 5 \
                   --tuning_metric mean_intent_slot \
                   --use_intent_context_attention \
                   --attention_embedding_size 200 \
